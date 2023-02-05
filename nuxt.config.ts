f2d5c938d62d0e788e1814e84a056c652728b580
radnive <radnive.dev@gmail.com>
2023-02-05 00:23:44 -0800

    Add light and dark themes (#3)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,16 @@ export default defineNuxtConfig({
         { rel: "icon", type: "image/png", href: "/favicon.png" }
       ]
     }
+  },
+  css: [
+    "~/assets/styles/colors.css",
+    "~/assets/styles/global.css"
+  ],
+  modules: [
+    "@nuxtjs/color-mode"
+  ],
+  colorMode: {
+    preference: "system", // default value of $colorMode.preference
+    fallback: "light" // fallback value if not system preference found
   }
 })
